b96ff602a2eaac99bbe5f84a67377d172eae7dcf
Brandon Lin <brandonlin9716@gmail.com>
2025-07-16 21:58:17 -0700

    Configure server socket admin ui

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
 import { app } from './app.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { attachSocketListeners } from './sockets/socketListeners.js';
+import { instrument } from '@socket.io/admin-ui';
 dotenv.config();
 
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [process.env.CLIENT_URI],
+        origin: [
+            process.env.SERVER_URI,
+            process.env.CLIENT_URI,
+            'https://admin.socket.io',
+        ],
+        credentials: true,
     },
+});
+
+instrument(io, {
+    auth: {
+        type: 'basic',
+        username: process.env.SOCKET_ADMIN_USERNAME,
+        password: await bcrypt.hash(process.env.SOCKET_ADMIN_PASSWORD, 10),
+    },
+    mode: 'development',
 });
 
 attachSocketListeners(io);
