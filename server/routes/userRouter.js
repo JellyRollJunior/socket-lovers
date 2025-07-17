e162769d20652387f990c359424c995f1c53bfb3
Brandon Lin <brandonlin9716@gmail.com>
2025-07-17 13:33:47 -0700

    Require token when retrieving users on GET /users

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { authenticateToken } from '../middleware/handleVerifyToken.js';
 import * as userController from '../controllers/userController.js';
 
 const userRouter = Router();
 
-userRouter.get('/', userController.getAllUsers);
+userRouter.get('/', authenticateToken, userController.getAllUsers);
 
 export { userRouter };
