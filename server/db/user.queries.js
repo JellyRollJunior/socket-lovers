8412abd2756c5938e81199ae99f86731469a478f
Brandon Lin <brandonlin9716@gmail.com>
2025-07-16 20:33:54 -0700

    Write queries to create and retrieve users

---
+++ server/db/user.queries.js
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client';
+import { DatabaseError } from '../errors/DatabaseError.js';
+
+const prisma = new PrismaClient();
+
+const getUserByUsername = async (username) => {
+    try {
+        const user = await prisma.user.findMany({
+            where: {
+                username,
+            },
+        });
+        return user;
+    } catch (error) {
+        throw new DatabaseError('Unable to fetch user');
+    }
+};
+
+const getUserById = async (id) => {
+    try {
+        const user = await prisma.user.findMany({
+            where: {
+                id,
+            },
+        });
+        return user;
+    } catch (error) {
+        throw new DatabaseError('Unable to fetch user');
+    }
+};
+
+const getAllUsers = async () => {
+    try {
+        const user = await prisma.user.findMany({
+            select: {
+                id: true,
+                username: true,
+            },
+            orderBy: {
+                username: 'asc',
+            },
+        });
+        return user;
+    } catch (error) {
+        throw new DatabaseError('Unable to fetch users');
+    }
+};
+
+const createUser = async (username, password) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password,
+            },
+        });
+        return user;
+    } catch (error) {
+        if (error.code == 'P2002') {
+            throw new DatabaseError('Username is already in use.', 409);
+        }
+        throw new DatabaseError('Unable to insert user');
+    }
+};
+
+export { getUserByUsername, getUserById, getAllUsers, createUser };
