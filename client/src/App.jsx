73e76745efc8f40fb36d7ef8910ff289b184a185
Brandon Lin <brandonlin9716@gmail.com>
2025-07-17 20:38:01 -0700

    Render routes using outlet element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,10 @@
-import { Chats } from './components/Chats.jsx';
-import { CreateChat } from './components/CreateChat.jsx';
-import { Login } from './components/Login.jsx';
-import { Messages } from './components/Messages.jsx';
+import { Outlet } from 'react-router';
 import { SocketProvider } from './contexts/SocketProvider.jsx';
 
 function App() {
   return (
     <SocketProvider>
-      <h1>Hello World!</h1>
-      <Chats />
-      <Messages />
-      <CreateChat />
-      <Login />
+      <Outlet />
     </SocketProvider>
   );
 }
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,19 +1,30 @@
 import App from './App.jsx';
+import { Chats } from './components/Chats.jsx';
+import { CreateChat } from './components/CreateChat.jsx';
 import { Login } from './components/Login.jsx';
 import { Messages } from './components/Messages.jsx';
 
+// create a page folder for all the pages later
 const routes = [
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <>
+            <Chats />
+            <CreateChat />
+          </>
+        ),
+      },
+      { path: '/chats/:chatId', element: <Messages /> },
+    ],
   },
   {
     path: '/login',
     element: <Login />,
-  },
-  {
-    path: '/chats/:chatId',
-    element: <App />,
   },
 ];
 
