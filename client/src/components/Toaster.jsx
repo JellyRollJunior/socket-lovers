2aa71043b01cbc6413bf92043fd4c5816a11b979
Brandon Lin <brandonlin9716@gmail.com>
2025-07-24 00:29:08 -0700

    Render toasts on screen

--- client/src/components/Toaster.jsx
+++ client/src/components/Toaster.jsx
@@ -1,8 +1,16 @@
+import { useContext } from 'react';
+import { ToastContext } from '../contexts/ToastProvider.jsx';
+
 const Toaster = () => {
+  const { toasts } = useContext(ToastContext);
   return (
-    <div>
-      <h1>I am a toaster</h1>
-    </div>
+    <ul className="absolute left-1/2 top-1">
+      {toasts && toasts.map((toast) => (
+          <li className="text-3xl font-bold text-red-400" key={toast.id}>
+            {toast.message}
+          </li>
+        ))}
+    </ul>
   );
 };
 
