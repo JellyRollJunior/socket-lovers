47869a5ce05ed517635b2f1bba1698c623e6c5d9
Brandon Lin <brandonlin9716@gmail.com>
2025-07-25 14:33:44 -0700

    Increase chat padding top. Make convo items use full width of chat

--- client/src/components/Chats.jsx
+++ client/src/components/Chats.jsx
@@ -9,24 +9,24 @@ const Chats = () => {
   const { username } = useContext(CurrentContext);
 
   return (
-    <section className="h-full pt-4">
-      <header className="flex justify-between pl-4 pr-6">
+    <section className="h-full pt-9">
+      <header className="flex justify-between pl-5 pr-7">
         <h2 className="text-3xl font-bold">{username}</h2>
         <button className="mt-auto">
           <img className="w-8" src={newChatIcon} alt="Create new chat icon" />
         </button>
       </header>
-      <div className='px-2'>
+      <div className='px-3'>
         <input
-          className="mt-3 h-9 w-full rounded-md border-2 border-b-black"
+          className="mt-3 h-11 w-full rounded-md border-2 border-b-black"
           type="text"
         />
       </div>
-      <h3 className="mt-5 pl-3 text-xl font-bold">Conversations</h3>
+      <h3 className="mt-5 pl-4 text-xl font-extrabold">Conversations</h3>
       <ul className="mt-2 overflow-scroll">
         {chats &&
           chats.map((chat) => (
-            <li key={chat.id} className="px-3 py-2">
+            <li key={chat.id} className="px-4 py-2">
               <Link
                 className="flex items-center gap-2"
                 to={`/chats/${chat.id}`}
