8a4b89ef87a44e549bb983ada1d853ae0d937fa4
Brandon Lin <brandonlin9716@gmail.com>
2025-07-26 12:57:14 -0700

    Add nav logout, home, and profile icons

--- client/src/components/NavigationPageWrapper.jsx
+++ client/src/components/NavigationPageWrapper.jsx
@@ -1,9 +1,27 @@
+import { Link } from 'react-router';
+import { useLogout } from '../hooks/useLogout.js';
+import logoutIcon from '../assets/svgs/logout.svg';
+import homeIcon from '../assets/svgs/home.svg';
+import accountIcon from '../assets/svgs/account.svg';
+
 const NavigationPageWrapper = ({ children }) => {
+  const { logout } = useLogout();
+
   return (
     <>
-      <div className="h-screen pb-16">{children}</div>
-      <nav className="fixed bottom-0 h-16 w-full border-2 border-black bg-gray-500">
-        I am a nav!
+      <div className="pb-13 h-screen">{children}</div>
+      <nav className="h-13 fixed bottom-0 flex w-full justify-around bg-gray-300">
+        <button onClick={() => logout()}>
+          <img className="w-8" src={logoutIcon} alt="Logout icon" />
+        </button>
+        <button>
+          <Link to="/">
+            <img className="w-8" src={homeIcon} alt="Home icon" />
+          </Link>
+        </button>
+        <button>
+          <img className="w-8" src={accountIcon} alt="Profile icon" />
+        </button>
       </nav>
     </>
   );
