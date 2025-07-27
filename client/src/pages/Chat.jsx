1d48a896d1d3b2531e6f9e85bf44b5fc199d8e2d
Brandon Lin <brandonlin9716@gmail.com>
2025-07-26 22:09:29 -0700

    Add timestamp message element

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns';
 import { useChat } from '../hooks/useChat.js';
 import { CurrentContext } from '../contexts/CurrentProvider.jsx';
 import { useJoinRoom } from '../hooks/useJoinRoom.js';
+import { Fragment } from 'react';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -20,7 +21,7 @@ const Chat = () => {
   // join room on mount
   useJoinRoom(chatId);
 
-  // scroll to bottom of messages
+  // scroll to bottom of messages on message change
   const scrollContainerRef = useRef(null);
   useEffect(() => {
     const element = scrollContainerRef.current;
@@ -47,17 +48,32 @@ const Chat = () => {
         >
           <ul className="flex flex-col gap-3">
             {messages &&
-              messages.map((message) => (
-                <li
-                  className={`max-w-4/5 w-fit rounded-3xl border-2 border-gray-200 px-5 py-2 ${message.sender.id == id && 'self-end bg-gray-200'}`}
-                >
-                  <h3>{message.content}</h3>
-                  <p
-                    className={`text-sm text-gray-500 ${message.sender.id == id && 'justify-self-end'}`}
+              messages.map((message, index) => (
+                <Fragment key={`${message.id}-wrapper`}>
+                  {/* if (last message time - current message time) >= 12hr, show timestamp element */}
+                  {(index == 0 ||
+                    new Date(message.sendTime) -
+                      new Date(messages[index - 1].sendTime) >=
+                      3200000) && (
+                    <li
+                      key={`${message.id}-timestamp`}
+                      className={`max-w-4/5 w-fit self-center rounded-3xl border-2 border-gray-100 bg-gray-50 px-5 py-2`}
+                    >
+                      {format(new Date(message.sendTime), 'EEEE LLLL do')}
+                    </li>
+                  )}
+                  <li
+                    key={message.id}
+                    className={`max-w-4/5 w-fit rounded-3xl border-2 border-gray-200 px-5 py-2 ${message.sender.id == id && 'self-end bg-gray-200'}`}
                   >
-                    {format(new Date(message.sendTime), 'h:mmaaa')}
-                  </p>
-                </li>
+                    <h3>{message.content}</h3>
+                    <p
+                      className={`text-sm text-gray-500 ${message.sender.id == id && 'justify-self-end'}`}
+                    >
+                      {format(new Date(message.sendTime), 'h:mmaaa')}
+                    </p>
+                  </li>
+                </Fragment>
               ))}
           </ul>
         </main>
