86d10c54080e3e7a2f1bb63b4bb35f1ef82c4394
Brandon Lin <brandonlin9716@gmail.com>
2025-07-27 00:43:24 -0700

    Style login page. Add socket lovers title

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router';
 import { login } from '../services/authApi.js';
 
 const Login = () => {
@@ -17,32 +18,49 @@ const Login = () => {
 
   return (
     <>
-      <h2>login</h2>
-      <form onSubmit={handleLogin}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          minLength={1}
-          maxLength={36}
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          minLength={1}
-          maxLength={36}
-          required
-        />
-        <button>Login</button>
-      </form>
+      <div className="flex h-screen flex-col items-center">
+        <h1 className="mt-36 text-5xl font-extrabold italic">Socket Lovers</h1>
+        <form
+          className="max-w-2xs mt-12 flex w-full flex-col"
+          onSubmit={handleLogin}
+        >
+          <input
+            className="border-1 mt-1 h-11 w-full rounded-md border-gray-400 bg-gray-200 pl-1.5"
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            minLength={1}
+            maxLength={36}
+            placeholder="Username"
+            required
+          />
+          <input
+            className="border-1 mt-3 h-11 w-full rounded-md border-gray-400 bg-gray-200 pl-1.5"
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            minLength={1}
+            maxLength={36}
+            placeholder="Password"
+            required
+          />
+          <div className="mt-8">
+            <button className="w-full rounded-xl bg-blue-400 px-5 py-2 text-white hover:bg-blue-500">
+              Log In
+            </button>
+          </div>
+        </form>
+        <footer className="mt-2">
+          Dont have an account?{' '}
+          <Link to="/signup" className="text-blue-400 hover:underline">
+            Sign up.
+          </Link>
+        </footer>
+      </div>
     </>
   );
 };
