af70d1ce3d09f60d12a2fac351e7e418efc61c50
Brandon Lin <brandonlin9716@gmail.com>
2025-08-02 14:26:28 -0700

    Add loading animation for ProfileModal

--- client/src/components/ProfileModal.jsx
+++ client/src/components/ProfileModal.jsx
@@ -1,11 +1,55 @@
 import { useUser } from '../hooks/useUser.js';
 import { ModalDialog } from './ModalDialog.jsx';
 import { Avatar } from './Avatar.jsx';
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'motion/react';
 
 const ProfileModal = ({ isOpen, closeFunction, userId }) => {
   const { user, isLoading } = useUser(userId);
 
-  // username, avatar, bio
+  if (isLoading) {
+    return (
+      <ModalDialog isOpen={isOpen} closeFunction={closeFunction}>
+        <div className="min-w-2xs flex flex-col items-center justify-center">
+          <motion.div
+            style={{ backgroundColor: '#f3f4f6' }}
+            animate={{ backgroundColor: '#d1d5db' }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: 0.8,
+              ease: 'easeInOut',
+            }}
+            className="size-32 shrink-0 rounded-full bg-gray-200"
+          ></motion.div>
+          <motion.h2
+            style={{ backgroundColor: '#f3f4f6' }}
+            animate={{ backgroundColor: '#d1d5db' }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: 0.8,
+              ease: 'easeInOut',
+            }}
+            className="mt-2 h-4 w-36 self-center rounded-sm bg-gray-200 text-xl font-bold"
+          ></motion.h2>
+          <div className="mt-2 self-start font-medium text-gray-500">Bio</div>
+          <motion.p
+            style={{ backgroundColor: '#f3f4f6' }}
+            animate={{ backgroundColor: '#d1d5db' }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: 0.8,
+              ease: 'easeInOut',
+            }}
+            className="mt-2 h-14 w-full self-center rounded-sm bg-gray-200 text-xl font-bold"
+          ></motion.p>
+        </div>
+      </ModalDialog>
+    );
+  }
+
   return (
     <ModalDialog isOpen={isOpen} closeFunction={closeFunction}>
       {user && (
