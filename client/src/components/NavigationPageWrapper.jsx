61f6db178adb4c19e245f5e077131876d9ff98d6
Brandon Lin <brandonlin9716@gmail.com>
2025-08-03 14:45:16 -0700

    Link profile button to /profile page. Hover styling for nav

--- client/src/components/NavigationPageWrapper.jsx
+++ client/src/components/NavigationPageWrapper.jsx
@@ -4,6 +4,7 @@ import logoutIcon from '../assets/svgs/logout.svg';
 import homeIcon from '../assets/svgs/home.svg';
 import homeFilledIcon from '../assets/svgs/home-filled.svg';
 import accountIcon from '../assets/svgs/account.svg';
+import accountFilledIcon from '../assets/svgs/account-filled.svg';
 
 const NavigationPageWrapper = ({ children }) => {
   const { logout } = useLogout();
@@ -13,11 +14,12 @@ const NavigationPageWrapper = ({ children }) => {
 
   return (
     <>
-      <nav className="fixed bottom-0 flex h-12 w-full items-center justify-around bg-gray-300 md:top-0 md:pt-10 md:h-full md:w-12 md:flex-col md:justify-start md:gap-8">
-        <button onClick={() => logout()}>
+      <nav className="fixed bottom-0 flex h-12 w-full items-center justify-around bg-gray-300 md:top-0 md:h-full md:w-12 md:flex-col md:justify-start md:gap-6 md:pt-10">
+        <button className="rounded-2xl px-1 py-1 hover:bg-gray-400"
+          onClick={() => logout()}>
           <img className="w-8" src={logoutIcon} alt="Logout icon" />
         </button>
-        <button className='md:mt-10'>
+        <button className="rounded-2xl px-1 py-1 hover:bg-gray-400 md:mt-10">
           <Link to="/">
             <img
               className="w-8"
@@ -26,8 +28,14 @@ const NavigationPageWrapper = ({ children }) => {
             />
           </Link>
         </button>
-        <button>
-          <img className="w-8" src={accountIcon} alt="Profile icon" />
+        <button className="rounded-2xl px-1 py-1 hover:bg-gray-400">
+          <Link to="/profile">
+            <img
+              className="w-8"
+              src={path == '/profile' ? accountFilledIcon : accountIcon}
+              alt="Profile icon"
+            />
+          </Link>
         </button>
       </nav>
       <div className="h-screen pb-12 md:pb-0 md:pl-12">{children}</div>
