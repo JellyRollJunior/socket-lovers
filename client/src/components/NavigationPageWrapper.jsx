5bb773b78f6673045600afb5975d32eda9f2fea3
Brandon Lin <brandonlin9716@gmail.com>
2025-08-08 00:45:37 -0700

    Add messages icon to nav bar

--- client/src/components/NavigationPageWrapper.jsx
+++ client/src/components/NavigationPageWrapper.jsx
@@ -5,13 +5,14 @@ import homeIcon from '../assets/svgs/home.svg';
 import homeFilledIcon from '../assets/svgs/home-filled.svg';
 import accountIcon from '../assets/svgs/account.svg';
 import accountFilledIcon from '../assets/svgs/account-filled.svg';
+import messagesIcon from '../assets/svgs/messages.svg';
+import messagesFilledIcon from '../assets/svgs/messages-filled.svg';
 
 const NavigationPageWrapper = ({ children }) => {
   const { logout } = useLogout();
   const location = useLocation();
 
   const path = location.pathname;
-
   return (
     <>
       <nav className="fixed bottom-0 flex h-12 w-full items-center justify-around bg-gray-300 md:top-0 md:h-full md:w-12 md:flex-col md:justify-start md:gap-6 md:pt-10">
@@ -28,6 +29,13 @@ const NavigationPageWrapper = ({ children }) => {
             />
           </Link>
         </button>
+        <div>
+          <img
+              className="w-8"
+              src={path.includes('chats') ? messagesFilledIcon : messagesIcon}
+              alt="Home icon"
+            />
+        </div>
         <button className="rounded-2xl px-1 py-1 hover:bg-gray-400">
           <Link to="/profile">
             <img
