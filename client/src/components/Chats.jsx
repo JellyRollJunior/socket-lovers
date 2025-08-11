53a4337e135d5477a23fc3b7e86ca72443c9fdde
Brandon Lin <brandonlin9716@gmail.com>
2025-08-11 16:21:00 -0700

    Add refresh button that rotates on hover

--- client/src/components/Chats.jsx
+++ client/src/components/Chats.jsx
@@ -3,6 +3,9 @@ import { CurrentContext } from '../contexts/CurrentProvider.jsx';
 import { ChatsListItem } from './ChatsListItem.jsx';
 import { ChatsLoading } from './ChatsLoading.jsx';
 import newChatIcon from '../assets/svgs/edit-square.svg';
+import refreshIcon from '../assets/svgs/refresh.svg';
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'motion/react';
 
 const Chats = ({ chats, isLoading, openNewChatModal }) => {
   const { id, username } = useContext(CurrentContext);
@@ -49,7 +52,26 @@ const Chats = ({ chats, isLoading, openNewChatModal }) => {
           placeholder="Search"
         />
       </div>
-      <h3 className="mt-5 pl-4 text-xl font-extrabold">Conversations</h3>
+      <div className="mt-5 flex items-center justify-between">
+        <h3 className="pl-4 text-xl font-extrabold">Conversations</h3>
+        <motion.button
+          initial={{ rotate: 0 }}
+          transition={{
+            duration: 0.8,
+            ease: 'easeInOut',
+          }}
+          whileHover={{
+            rotate: 360,
+            transition: {
+              duration: 1,
+              ease: 'easeInOut',
+            },
+          }}
+          className="mr-6.5 rounded-2xl px-1 py-1 hover:bg-gray-200"
+        >
+          <img src={refreshIcon} alt="Refresh icon" />
+        </motion.button>
+      </div>
       <main className="scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full mt-2 flex-1 overflow-y-scroll">
         <ul className="scrollbar-track-black">
           {isLoading && <ChatsLoading />}
